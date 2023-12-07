2bc1042c9159a75507e3c865eae30289175cb59d
Davy Baetens <davy.baetens@hotmail.com>
2023-12-07 19:54:29 +0100

    som en product

---
+++ oefening2.py
@@ -0,0 +1,7 @@
+getal_1 = int(input("Wat is getal 1?"))
+getal_2 = int(input("Wat is getal 2?"))
+
+som = (getal_1 + getal_2)
+product = (getal_1 * getal_2)
+
+print(f"De som van beide getallen is {som} en het product is {product}")
